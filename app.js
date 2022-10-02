495e0200379aa42eae25bad1afc1f0d7b873f00b
Juan <juan.carosella@gmail.com>
2022-10-02 13:21:36 -0300

    jaja

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const fs = require("fs");
+const port = 3000
+
+app.get('/', (req, res) => {
+  fs.readFile('cartas.json', (err, data) => {
+    if (err) throw err;
+    let response = JSON.parse(data);
+    res.send(response);
+});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
