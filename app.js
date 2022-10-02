6e2b8c4912df39f20d9d3c19e576c5c92701213a
Juan <juan.carosella@gmail.com>
2022-10-02 14:57:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const fs = require("fs");
 const port = 3000
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   fs.readFile('cartas.json', (err, data) => {
